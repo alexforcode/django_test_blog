5c4237cdb3348b9694f727bc4cfa8202bc5ede2e
Alex Code <alex.for.code@gmail.com>
2020-04-07 23:20:56 +0300

    Add Post model to admin

--- test_blog/blog/admin.py
+++ test_blog/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'publish'
+    list_display = ('title', 'slug', 'author', 'created', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    ordering = ('-publish', 'status')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    search_fields = ('title', 'body')
