4cfd7ad6c1614c36abe926a12476b29904c7abb8
Alex Code <alex.for.code@gmail.com>
2020-04-24 15:37:56 +0300

    Add similar posts to detail post view

--- test_blog/blog/views.py
+++ test_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 
 from taggit.models import Tag
@@ -22,9 +23,9 @@ def post_list(request, tag_slug=None):
     try:
         posts = paginator.page(page)
     except EmptyPage:
-        posts = paginator.page(1)
-    except PageNotAnInteger:
         posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
 
     context = {'page': page, 'posts': posts, 'tag': tag}
     return render(request, 'blog/post/list.html', context)
@@ -49,11 +50,16 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     context = {
         'post': post,
         'comments': comments,
         'new_comment': new_comment,
-        'comment_form': comment_form
+        'comment_form': comment_form,
+        'similar_posts': similar_posts
     }
     return render(request, 'blog/post/detail.html', context)
 
